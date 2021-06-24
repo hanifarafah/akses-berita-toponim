69e092e6a221152b8913866582513053531f5282
hanifarafah <hanifam44@gmail.com>
2021-06-24 10:53:33 +0700

    menambahkan halaman web ke repositori

---
+++ kelasToponim.js
@@ -0,0 +1,40 @@
+var kelasUnsur = {
+  "Toponim Bangunan" : ["Bangunan", "Gedung"],
+  "Toponim Industri" : ["Industri Manufaktur"],
+  "Toponim Olahraga" : ["Arena Atletik dan Olah Raga", "Arena Balap Otomotif", "Arena Balap Sepeda", "Arena Kolam Renang", "Arena Olah Raga", "Arena Pacuan Kuda", "Bangunan Olah Raga", "Jalur Golf", "Olah Raga Air", "Stadion", "Tribun", "Velodrome"],
+  "Toponim Pariwisata, Seni dan Budaya" : ["Bioskop", "Budaya", "Cagar Alam", "Candi", "Galeri", "Gapura", "Gardu Pandang", "Hostel", "Hotel", "Kandang", "Kebun Binatang", "Konser", "Losmen", "Menara", "Monumen", "Motel", "Museum", "Olah Raga Lainnya", "Pameran", "Pariwisata", "Pasar Seni", "Penangkaran", "Pertemuan", "Peternakan", "Prasasti", "Rekreasi Budaya", "Rekreasi Pantai", "Rekreasi Pegunungan", "Restauran", "Rumah Kaca Taman Botani", "Sangkar Binatang", "Seni", "Situs Purbakala", "Taman Botani", "Taman Margasatwa", "Taman Sumber Air Panas", "Teater Seni", "Tempat Hiburan", "Tempat Makan", "Tugu"],
+  "Toponim Pemerintahan" : ["Istana", "Istana Negara", "Kantor Bupati", "Kantor Camat", "Kantor DPRD", "Kantor Gubernur", "Kantor Kepala Desa", "Kantor Lembaga Negara", "Kantor Lurah", "Kantor Menteri", "Kantor Pemerintah Lainnya", "Kantor Presiden", "Kantor Wakil Presiden", "Kantor Wali Kota", "Kementerian", "LPNK", "Lembaga Pemasyarakatan"],
+  "Toponim Pemerintahan Negara Asing" : ["Kantor Pemerintah Asing Lainnya", "Kedutaan Besar", "Konsulat", "Pusat Kebudayaan Asing"],
+  "Toponim Pendidikan dan IPTEK" : ["Laboratorium Pendidikan", "Observatorium", "Pendidikan", "Pendidikan Anak Usia Dini", "Pendidikan Dasar", "Pendidikan Keagamaan", "Pendidikan Kedinasan", "Pendidikan Khusus", "Pendidikan Luar Sekolah", "Pendidikan Menengah Pertama", "Pendidikan Menengah Umum", "Pendidikan Tinggi", "Penelitian", "Penelitian Lainnya", "Perpustakaan"],
+  "Toponim Perairan" : ["Air Terjun", "Alur Sungai", "Bendung", "Bendungan", "Danau", "Delta", "Empang", "Hulu", "Jeram", "Kanal", "Kolam", "Kuala", "Laguna", "Laut", "Mata Air", "Muara", "Muara", "Perairan Lainnya", "Pintu Air", "Rawa", "Riam", "Saluran Air", "Samudera", "Selat", "Situ", "Sungai", "Tambak", "Tanggul", "Teluk", "Terusan", "Waduk"],
+  "Toponim Perekonomian dan Perdagangan" : ["Cargo", "Efek", "Grosir", "Induk", "Kantor Bank", "Kantor Bursa Saham", "Kantor Keuangan Lainnya", "Kantor Pegadaian", "Kantor Penukaran Uang Asing", "Komersial Terpadu", "Mal", "Pasar Eceran", "Pusat Bisnis dan Perdagangan Lainnya", "Pusat Pelelangan Bahan Pokok Pangan", "Pusat Pelelangan Produk Industri", "Pusat Perdagangan Tradisional", "Pusat Perdagangan dan Niaga Kota", "Pusat Pergudangan", "Pusat Perkantoran Bisnis", "Rumah Kantor", "Rumah Toko", "Terminal Peti Kemas", "Toserba"],
+  "Toponim Peribadatan" : ["Gereja", "Kelenteng", "Masjid", "Peribadatan", "Pura", "Sosial Lainnya", "Vihara"],
+  "Toponim Permakaman" : ["Kantor Pemakaman", "Krematorium", "Pemakaman Bukan Umum", "Pemakaman Khusus", "Pemakaman Umum", "Tempat Penyimpanan Jenazah"],
+  "Toponim Permukiman" : ["Dusun", "Ibukota Daerah Istimewa", "Ibukota Desa", "Ibukota Kabupaten", "Ibukota Kecamatan", "Ibukota Kelurahan", "Ibukota Kota", "Ibukota Negara", "Ibukota Provinsi", "Kampung", "Permukiman Lainnya", "Properti Real Estate", "Properti Tumpang Susun", "Rumah Adat", "Rumah Hunian Lainnya", "Rumah Komplek"],
+  "Toponim Pertahanan dan Keamanan" : ["Instalasi TNI AD", "Instalasi TNI AL", "Instalasi TNI AU", "Kantor Pertahanan Keamanan Lainnya", "Kantor Polisi", "Pangkalan Transportasi TNI", "Pos Keamanan"],
+  "Toponim Pertambangan Mineral" : ["Pertambangan"],
+  "Toponim Relief" : ["Bukit", "Dataran Tinggi", "Goa", "Gosong", "Gunung", "Karang", "Kawah", "Kepulauan", "Lembah", "Patahan", "Pegunungan", "Pulau", "Puncak", "Relief Lainnya", "Semenanjung", "Tanjung", "Ujung"],
+  "Toponim Sarana Kesehatan" : ["Fasilitas Kesehatan Lainnya", "Poliklinik", "Polindes", "Posyandu", "Puskesmas", "Puskesmas Pembantu", "Rumah Sakit Khusus", "Rumah Sakit Umum"],
+  "Toponim Sosial" : ["Rumah Panti Asuhan Anak Yatim Piatu", "Rumah Panti Jompo", "Rumah Panti Lainnya"],
+  "Toponim Transportasi" : ["Angkutan Kendaraan Lainnya", "Balai Yasa Kereta Api", "Depo Kendaraan (Pool)", "Depo Kereta Api", "Dermaga Laut", "Dermaga Sungai dan Danau", "Halte", "Halte Kereta Api", "Hanggar Pesawat Udara", "Helikopter Pad Gedung", "Helikopter Pad Tanah", "Jalan", "Jalan Kereta Api", "Jembatan", "Menara Suar", "Pelabuhan Laut", "Pelabuhan Penyeberangan", "Pelabuhan Sungai dan Danau", "Pelabuhan Udara Domestik", "Pelabuhan Udara Internasional", "Pelabuhan Udara Non Reguler", "Pelabuhan Udara Perintis", "Perhentian", "Perhentian Bus", "Stasiun Kereta Api", "Tempat Parkir Kendaraan Bermotor", "Tempat Parkir Tumpang Susun", "Terminal Bus", "Terowongan", "Transportasi Darat Lainnya", "Transportasi Perairan Lainnya"],
+  "Toponim Utilitas" : ["Depo Bahan Bakar Gas", "Depo Bahan Bakar Minyak", "Gardu Listrik", "Kantor Gas Negara", "Kantor PLN", "Kantor Pengiriman Paket", "Kantor Perusahaan Air Minum", "Kantor Pos", "Kantor Radio", "Kantor Telepon", "Kantor Televisi", "Lingkungan", "Menara Air Minum", "Menara Pemancar Radio", "Menara Pemancar Televisi", "Menara Telepon", "Pembangkit Listrik Tenaga Air", "Pembangkit Listrik Tenaga Angin", "Pembangkit Listrik Tenaga Diesel", "Pembangkit Listrik Tenaga Gas", "Pembangkit Listrik Tenaga Lainnya", "Pembangkit Listrik Tenaga Nuklir", "Pembangkit Listrik Tenaga Panas Bumi", "Pembangkit Listrik Tenaga Surya", "Pembangkit Listrik Tenaga Uap", "Penanganan Air Minum Lainnya", "Pengolahan Air Minum", "Pengolahan Bahan Bakar Gas", "Pengolahan Bahan Bakar Minyak", "Stasiun Bumi", "Stasiun Pasut", "Stasiun Pompa Bahan Bakar Gas", "Stasiun Pompa Bahan Bakar Umum", "Sumber Air Minum", "Telekomunikasi Lainnya", "Tempat Pembuangan Akhir Sampah", "Tempat Pembuangan Lainnya", "Tempat Penampungan Barang Bekas", "Warung Internet", "Warung Telekomunikasi"],
+  "Toponim Vegetasi dan Lahan Terbuka" : ["Gambut", "Hutan Bakau", "Hutan Rawa", "Hutan Rimba", "Hutan Tanaman Industri", "Kebun", "Lapangan Diperkeras", "Lumpur", "Mangrove", "Padang Rumput", "Perkebunan", "Permukaan", "Taman", "Tanaman Campur", "Vegetasi Budidaya Lainnya", "Vegetasi Non Budidaya Lainnya"],
+  "Toponim Wilayah Administrasi" : ["Daerah Administratif Lainnya", "Daerah Istimewa", "Desa", "Kabupaten", "Kecamatan", "Kelurahan", "Kota", "Negara", "Provinsi"]
+}
+
+window.onload = function() {
+    var kelasToponim = document.getElementById("klasifikasi-toponim");
+    var namaUnsur = document.getElementById("nama-unsur");
+
+    for (var x in kelasUnsur) {
+      kelasToponim.options[kelasToponim.options.length] = new Option(x, x);
+    }
+
+    kelasToponim.onchange = function() {
+      namaUnsur.length = 1;
+      var y = kelasUnsur[this.value];
+      for (var i = 0; i < y.length; i++) {
+        namaUnsur.options[namaUnsur.options.length] = new Option(y[i], y[i]);
+      }
+    }
+}
